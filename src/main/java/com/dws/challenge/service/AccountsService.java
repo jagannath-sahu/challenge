8cc7d2a153ea13464832d56bc3fe234e1807c787
Jagannath Sahu <jagannathsahu@Jagannaths-MacBook-Pro.local>
2023-07-28 18:59:09 +0530

    added lock using lock ordering to avoid deadlocks and achieve parallelism

--- src/main/java/com/dws/challenge/service/AccountsService.java
+++ src/main/java/com/dws/challenge/service/AccountsService.java
@@ -11,16 +11,22 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 @Service
 public class AccountsService {
 
 	@Getter
 	private final AccountsRepository accountsRepository;
-	
+
 	@Getter
 	private final NotificationService notificationService;
 	
+	private final Map<String, Lock> accountsLocks = new ConcurrentHashMap<>();
+
 	@Autowired
 	public AccountsService(AccountsRepository accountsRepository, NotificationService notificationService) {
 		this.accountsRepository = accountsRepository;
@@ -36,39 +42,43 @@ public class AccountsService {
 	}
 
 	public void transfer(String accountFromId, String accountToId, BigDecimal amount) {
-	    Account accountFrom = accountsRepository.getAccount(accountFromId);
-	    Account accountTo = accountsRepository.getAccount(accountToId);
+        Account accountFrom = accountsRepository.getAccount(accountFromId);
+        Account accountTo = accountsRepository.getAccount(accountToId);
 
-	    if (accountFrom == null || accountTo == null) {
-	        throw new IllegalArgumentException("One or both account(s) do not exist.");
-	    }
+        if (accountFrom == null || accountTo == null) {
+            throw new IllegalArgumentException("One or both account(s) do not exist.");
+        }
 
-	    if (accountFrom.getBalance().compareTo(amount) < 0) {
-	        throw new IllegalArgumentException("Insufficient balance in the account to transfer.");
-	    }
+        if (accountFrom.getBalance().compareTo(amount) < 0) {
+            throw new IllegalArgumentException("Insufficient balance in the account to transfer.");
+        }
 
-	    CompletableFuture<Void> transferFromTo = CompletableFuture.runAsync(() -> {
-	        synchronized (accountFrom) {
-	            accountFrom.setBalance(accountFrom.getBalance().subtract(amount));
-	            accountsRepository.updateAccount(accountFrom);
-	            notificationService.notifyAboutTransfer(accountFrom,
-	                    "Amount " + amount + " transferred to Account: " + accountTo.getAccountId());
-	        }
-	    });
+        // Acquire locks in a consistent order to prevent deadlocks
+        Lock lock1 = accountsLocks.computeIfAbsent(accountFromId, k -> new ReentrantLock());
+        Lock lock2 = accountsLocks.computeIfAbsent(accountToId, k -> new ReentrantLock());
 
-	    CompletableFuture<Void> transferToFrom = CompletableFuture.runAsync(() -> {
-	        synchronized (accountTo) {
-	            accountTo.setBalance(accountTo.getBalance().add(amount));
-	            accountsRepository.updateAccount(accountTo);
-	            notificationService.notifyAboutTransfer(accountTo,
-	                    "Amount " + amount + " received from Account: " + accountFrom.getAccountId());
-	        }
-	    });
-	   
-	    try {
-	        CompletableFuture.allOf(transferFromTo, transferToFrom).get();
-	    } catch (InterruptedException | ExecutionException e) {
-	        throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Transfer failed.");
-	    }
-	}
+        if (accountFromId.compareTo(accountToId) < 0) {
+            lock1.lock();
+            lock2.lock();
+        } else {
+            lock2.lock();
+            lock1.lock();
+        }
+
+        try {
+            accountFrom.setBalance(accountFrom.getBalance().subtract(amount));
+            accountTo.setBalance(accountTo.getBalance().add(amount));
+
+            accountsRepository.updateAccount(accountFrom);
+            accountsRepository.updateAccount(accountTo);
+
+            notificationService.notifyAboutTransfer(accountFrom,
+                    "Amount " + amount + " transferred to Account: " + accountTo.getAccountId());
+            notificationService.notifyAboutTransfer(accountTo,
+                    "Amount " + amount + " received from Account: " + accountFrom.getAccountId());
+        } finally {
+            lock2.unlock();
+            lock1.unlock();
+        }
+    }
 }
